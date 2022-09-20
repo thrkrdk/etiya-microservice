bcd4ba9985c0a37c4b6b82832cd6abd032b26791
Tahir KARDAK <tahir.kardak@etiya.com>
2022-09-20 14:48:29 +0300

    Initial commit

---
+++ node-configuration/src/main/java/org/training/etiya/ms/configuration/NodeConfigurationApplication.java
@@ -0,0 +1,13 @@
+package org.training.etiya.ms.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodeConfigurationApplication.class, args);
+    }
+
+}
---
+++ node-configuration/src/test/java/org/training/etiya/ms/configuration/NodeConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training.etiya.ms.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
